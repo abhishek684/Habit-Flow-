1aa14b6cafdaccf5a71427b3f693e83cbe1f6288
Abhishek <abhishek@example.com>
2026-02-21 15:20:40 +0530

    Update API baseURL to use live Render backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://habit-flow-p8mk.onrender.com/api',
 });
 
 // Attach JWT token to every request
